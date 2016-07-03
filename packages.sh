03023c1d8667178cba320689f9e9ef1ccb325476
Yeri Tiete <yeri@flatturtle.com>
2016-07-03 14:57:52 +0200

    curl missing

--- packages.sh
+++ packages.sh
@@ -3,6 +3,6 @@
 # configure which packages to install. Default should be ok for a minimal install
 # Any vendor or company specific should be defined in 'more.sh'.
 
-PACKAGES="git-core nano rsyslog binutils ca-certificates wget libreadline6 dialog module-init-tools apt-utils isc-dhcp-client net-tools locales console-common console-setup ntpdate openssh-server bsd-mailx postfix inetutils-tools inetutils-ping psmisc"
+PACKAGES="git-core curl nano rsyslog binutils ca-certificates wget libreadline6 dialog module-init-tools apt-utils isc-dhcp-client net-tools locales console-common console-setup ntpdate openssh-server bsd-mailx postfix inetutils-tools inetutils-ping psmisc"
 
 # EOF
